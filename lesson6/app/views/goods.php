43562afcd934286777be17ef79f5b4f40b1cf39d
Sergey Berdnikov <30774859+SBerdnikau@users.noreply.github.com>
2019-07-29 13:11:14 +0300

    lesson 6

---
+++ lesson6/app/views/goods.php
@@ -0,0 +1,10 @@
+<?php /** @var array $goods */ ?>
+<h1>Товары</h1>
+
+<?php foreach ($goods as $good): /** App\models\Good $good */
+    echo <<<php
+<img src="{$good->address}">    
+<h2>$good->name </h2>
+<a href="?id=$good->id"> Подробнее </a>
+php;
+endforeach; ?>
---
+++ lesson6/app/views/layouts/main.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <ul>
+        <li><a href="/user/users">Пользователи</a></li>
+        <li><a href="/good/goods">Товары</a></li>
+    </ul>
+</div>
+<div class="content container">
+    <?= $content ?>
+</div>
+</body>
+</html>
